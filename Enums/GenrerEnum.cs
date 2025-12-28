ad7e13af9ab0121b9d1f5e3eab66ae9c3a39ef39
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-12-28 16:31:48 -0300

    creating the user model

---
+++ Enums/GenrerEnum.cs
@@ -0,0 +1,9 @@
+namespace divino_visual_api.Enums
+{
+    public enum GenrerEnum
+    {
+        Male,
+        Female,
+        Other
+    }
+}
---
+++ Enums/TypeUserEnum.cs
@@ -0,0 +1,8 @@
+namespace divino_visual_api.Enums
+{
+    public enum TypeUserEnum
+    {
+        Customer,
+        SalonAdministrator
+    }
+}
