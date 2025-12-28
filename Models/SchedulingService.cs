523618b0a3a21a80deb7acd82b5f85b3c935ee3e
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-12-28 17:22:00 -0300

    creating the scheduling service model

---
+++ Models/SchedulingService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace divino_visual_api.Models
+{
+    public class SchedulingService
+    {
+        public int Id { get; set; }
+        public int SchedulingId { get; set; }
+        public int ServiceId { get; set; }
+        public decimal ServicePrice { get; set; }
+        public int ServiceDurationInMinutes { get; set; }
+    }
+}
