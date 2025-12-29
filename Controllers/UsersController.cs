248937680b3dc4a4abd43a2c664fa307dbb193a6
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-12-29 19:47:50 -0300

    creating the users controller

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using divino_visual_api.Dtos.User;
+using divino_visual_api.Services.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace divino_visual_api.Controllers;
+
+[ApiController]
+[Route("/api/v1/users")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+    
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserById(int id)
+    {
+        return Ok(await _userService.GetUserById(id));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        return Ok(await _userService.GetAllUsers());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser(CreateUserDto createUserDto)
+    {
+        return Ok(await _userService.CreateUser(createUserDto));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser(int id, UpdateUserDto updateUserDto)
+    {
+        return Ok(await _userService.UpdateUser(id, updateUserDto));
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        return Ok(await _userService.DeleteUser(id));
+    }
+}
